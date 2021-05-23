ee224d7c7f4b52396943c3a75930d0b95d03aa44
Ramon Ramon <ramonsramon24@gmail.com>
2021-05-23 14:18:01 -0700

    Update to Angular 11

--- src/app/thank-you/thank-you.component.spec.ts
+++ src/app/thank-you/thank-you.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ThankYouComponent } from './thank-you.component';
 
@@ -6,7 +6,7 @@ describe('ThankYouComponent', () => {
   let component: ThankYouComponent;
   let fixture: ComponentFixture<ThankYouComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ThankYouComponent ]
     })
